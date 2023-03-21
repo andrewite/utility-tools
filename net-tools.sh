f1cd6e54751d3cb5f20826c3843b9154de364304
Live session user <andrewite@gmail.com>
2023-03-21 08:15:25 +0000

    Add New Function to pass Argument with space or special char

--- net-tools.sh
+++ net-tools.sh
@@ -6,11 +6,10 @@ Help()
 
 python_web_server_in_one_line()
 {
+  WWW_PORT=$(echo $1 $WWW_PORT 8080 | cut -d\  f1 )
+
   if [ ! "$WWW_BASE" ] ;then
     export WWW_BASE=$HOME 
-  fi
-  if [ ! "$WWW_PORT" ] ;then
-    export WWW_PORT=8080 
   fi
   if [ ! "$WWW_LOG" ] ;then
     export WWW_LOG=/dev/null
@@ -25,12 +24,14 @@ python_web_server_in_one_line()
     export WWW_FUN_PROCESS=\#echo\ internal-testing-code\;
   fi
 
+### BELOW LINE CAN BE PASTED IN SHELL WINDOW TO RUN A WEB SERVER
+
 WWW_PORT=$(echo $WWW_PORT 8080 | cut -d\  -f1 );echo -e "import os\\nimport SocketServer\\nclass ThreadingTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):\\n pass\\nclass Socks5Server(SocketServer.StreamRequestHandler):\\n def Read(self):\\n  Method=\"\"\\n  while True :\\n   c=self.connection.recv(1)\\n   if c == ' ' :\\n    break \\n   Method += c\\n  return Method\\n def handle(self):\\n  print 'connection from ', self.client_address\\n  sock = self.connection\\n  Method=self.Read()\\n  GETSTR=self.Read()\\n  pip = os.popen(\" $WWW_FUN_PROCESS \")\\n  pip = os.popen(\"export GETSTR=\" + GETSTR + \";              bash -c \\\" Head() { echo HTTP/1.1 200 OK; echo Cache-Control: max-age=170100100 ; } ; GETSTRFILE=\\\$WWW_BASE\\\$(echo \\\$GETSTR | sed s/?.*// | sed s/%%/%25/g | sed s/%/%%x/g | tr % \\\\\\\\\\\\\\\\\  2>/dev/null | xargs -i echo -e {} | sed \\\\\\\\\\\"s#.*/\\\\.\\\\./.*#/try_to_get_parent_error.html#\\\\\\\\\\\" ) ;  export WWW_REQUEST_FILE=\\\$(echo \\\$GETSTRFILE | sed s/%/\\\\\\\\x/g |xargs -i echo -e {} ) ; echo \\\$(date)  \\\$GETSTR >> \\\$WWW_LOG ; if echo \\\$GETSTR | grep -i [.]css\\\\\\\\$  > /dev/null ; then echo HTTP/1.1 200 OK; echo content-type: text/css ;echo; cat \\\$GETSTRFILE ; elif [ \\\\\\\\\\\"\\\$WWW_CGI_FUN_CMD\\\\\\\\\\\" ] ;then eval \\\$WWW_CGI_FUN_CMD ;  else Head; export WWW_REQUEST_URI=\\\$GETSTR; eval \\\$WWW_FUN_GET_MIMETYPE ; echo ;  echo \\\$WWW_REQUEST_FILE | xargs -i cat {}; fi \\\"             \" )\\n  while True:\\n   data=pip.read(1024)\\n   if data.__len__()==0 :\\n    pip.close()\\n    break \\n   sock.send(data)\\nserver = ThreadingTCPServer((\"\", $WWW_PORT), Socks5Server)\\nserver.allow_reuse_address = True\\nserver.serve_forever()\\n\\n" | python
 
 }
 
 
-WebServerCGIProcess()
+WebServerRequestProcess()
 {
   GETSTR=$1
   if [ ! "$GETSTR" ] ; then return ; fi
@@ -51,12 +52,13 @@ WebServerCGIProcess()
     echo $WWW_REQUEST_FILE | xargs -i cat {}; 
  fi 
 
-env
 }
 
-python_web_simple_server()
+python_nc_server()
 { 
-  WWW_PORT=$(echo $WWW_PORT 8080 | cut -d\  -f1 );
+  LISTEN_PORT=$(echo $1 8080 | cut -d\  -f1 );
+  SERVER_PROCESS_CMD="$2";
+
   echo  "import os
 import SocketServer
 class ThreadingTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
@@ -75,25 +77,53 @@ class Socks5Server(SocketServer.StreamRequestHandler):
   sock = self.connection
   Method=self.Read()
   GETSTR=self.Read()
-  pip = os.popen(\" $0 WebServerCGIProcess \" + GETSTR  )
+  pip = os.popen(\" $SERVER_PROCESS_CMD \" + GETSTR  )
   while True:
    data=pip.read(1024)
    if data.__len__()==0 :
     pip.close()
     break
    sock.send(data)
-server = ThreadingTCPServer((\"\", $WWW_PORT), Socks5Server)
+server = ThreadingTCPServer((\"\", $LISTEN_PORT), Socks5Server)
 server.allow_reuse_address = True
 server.serve_forever()" | python
 }
 
+python_web_simple_server()
+{ 
+  WWW_PORT=$(echo $1 $WWW_PORT 8080 | cut -d\  -f1 );
+  python_nc_server $WWW_PORT  " \\\"$0\\\" WebServerRequestProcess  "
+}
+
 python_web()
 {
+  shift
   python_web_server_in_one_line $*
 }
 
+
+StrForBashUsed()
+{
+  SedSpecialChar= ;  SedAndShellSpecialChar= ;
+  NeedBackSlashSpecialChar="\\\$" ## \\ Should be at First 
+  OnlyShellSpecialChar="\"\`?*'&#<>()!{|};"
+  (if [ "$1" ] ;then echo "$1" 
+   else head -1 ; fi ) |
+   sed  $(echo $NeedBackSlashSpecialChar | sed s/./s\\/\\\\\&\\/\\\\\\\\\\\&\\/g\;/g ) |
+   sed  $(echo $OnlyShellSpecialChar   | sed s/./s\\/\&\\/\\\\\\\\\\\&\\/g\;/g )     |
+   sed    s/\ /\\\\\ /g 
+
+}
+
+
 if [ "$1" ] ; then
-  $*
+
+  ## $*
+
+  CMDSTR=$1 ; 
+  while [ "$2" ] ;do CMDSTR=$CMDSTR\ \'$(echo $2 | sed s/\'/\'\\\\\'\'/g )\';shift;done
+  eval $CMDSTR
+
   exit $?
 else
  Help
